9ce4fd6d5bd79818d3207f6fae8b6c5fe10d1c6f
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-05 22:01:56 -0600

    commit tree afbd094f2d547b09a02fb4937b140a35280d4bcc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false;
 const toyCollection = document.getElementById('toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -14,6 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const grabSubmit = document.getElementsByClassName("submit")[0]
+
+  grabSubmit.addEventListener("click", newToy);
 });
 
 
@@ -56,4 +61,74 @@ function createCard(toy) {
   card.appendChild(para)
   card.appendChild(button)
 
+  button.addEventListener('click', (e) => updateLike(e, toy)) //callback funct.
+  // review pod video to see how addEvent arg works
 }
+
+function updateLike(e, toy) {
+  // needs to increase w/ every click
+  // capture how many likes currently 
+  // update innerText to rep new total
+  // update db with new total
+  toy.likes++
+  // let totalLikes = toy.likes
+  // get toy.likes from passing in through in addEvent
+  //totalLikes++;
+  //toy.likes = totalLikes
+  // need to reset amount to update each time new like is added
+  e.target.parentElement.querySelector('p').innerText = `${toy.likes} likes`
+  // from target (what was clicked) goes up DOM tree to parent
+  // then back down to select p tag
+  // then updates innerText with new total likes
+
+  // need to update db
+
+  const configObj = {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify( {
+      "likes": toy.likes
+    })
+  }
+
+  //const likesInfo = { "likes": toy.likes.value }
+
+  fetch(`http://localhost:3000/toys/${toy.id}`, configObj)
+  .then(resp => resp.json())
+  .then(json => console.log(json))
+
+  
+}
+
+// adding a new toy
+
+
+
+function newToy(event) {
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "name": document.getElementsByName("name")[0].value,
+      "image": document.getElementsByName("image")[0].value,
+      "likes": 0
+      // "name": "Jessie",
+      // "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+      // "likes": 0 
+    })
+  }
+
+  fetch("http://localhost:3000/toys", configObj)
+  .then(resp => resp.json())
+  .then(json => createCard(json))
+}
+
+
+
